f569ad3149c4ba52fe480b0342b660def3dbed3c
obaikoko <jesseobinna7@gmail.com>
2023-04-12 02:47:34 +0100

    inital commit

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'vhttps://goalsetter-h114.onrender.com/api/users/';
+const API_URL = 'https://goalsetter-h114.onrender.com/api/users/';
 
 const register = async (userData) => {
   const response = await axios.post(API_URL, userData);
--- src/features/goals/goalService.js
+++ src/features/goals/goalService.js
@@ -1,59 +1,58 @@
-import axios from "axios";
+import axios from 'axios';
 
 const API_URL = 'https://goalsetter-h114.onrender.com/api/goals';
 
-const createGoal = async(goalData, token) => {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }
+const createGoal = async (goalData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-    const response = await axios.post(API_URL, goalData, config)
+  const response = await axios.post(API_URL, goalData, config);
 
-    return response.data
-}
-const getGoals = async(token) => {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }  
- 
-    const response = await axios.get(API_URL, config)
+  return response.data;
+};
+const getGoals = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-    return response.data
-}
+  const response = await axios.get(API_URL, config);
 
-const deleteGoal = async(goalId, token) => {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }  
- 
-    const response = await axios.delete(API_URL + goalId, config) 
+  return response.data;
+};
 
-    return response.data
-}
-const updateGoal = async(updGoalId, token) => {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }  
- 
-    const response = await axios.put(API_URL + updGoalId, config) 
+const deleteGoal = async (goalId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-    return response.data
-}
+  const response = await axios.delete(API_URL + goalId, config);
 
+  return response.data;
+};
+const updateGoal = async (updGoalId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.put(API_URL + updGoalId, config);
+
+  return response.data;
+};
 
 const goalService = {
-    createGoal,
-    getGoals,
-    deleteGoal,
-    updateGoal
-} 
+  createGoal,
+  getGoals,
+  deleteGoal,
+  updateGoal,
+};
 
-export default goalService
+export default goalService;
