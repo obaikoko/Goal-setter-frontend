8225821c7bc5bea45e75e2ff530f631970b076a0
obaikoko <jesseobinna7@gmail.com>
2023-04-12 02:42:09 +0100

    inital commit

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,29 +1,28 @@
-import axios from 'axios'
-const API_URL = 'api/users/'
+import axios from 'axios';
+const API_URL = 'http://localhost:5000/api/users/';
 
 const register = async (userData) => {
-const response = await axios.post(API_URL, userData)
-if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data))
-}
-return response.data
-}
+  const response = await axios.post(API_URL, userData);
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data));
+  }
+  return response.data;
+};
 const login = async (userData) => {
-    const response = await axios.post(API_URL + 'login', userData)
-    if (response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data))
-    }
-    return response.data
-}
+  const response = await axios.post(API_URL + 'login', userData);
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data));
+  }
+  return response.data;
+};
 const logout = () => {
-    localStorage.removeItem('user')
-}
- 
+  localStorage.removeItem('user');
+};
 
 const authService = {
-    register,
-    login,
-    logout
-}
- 
-export default authService
+  register,
+  login,
+  logout,
+};
+
+export default authService;
--- src/features/goals/goalService.js
+++ src/features/goals/goalService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = '/api/goals/'
+const API_URL = 'http://localhost:5000/api/goals';
 
 const createGoal = async(goalData, token) => {
     const config = {
