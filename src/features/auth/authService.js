a3254e59b702703cd0f5d36eeda8e2f5d46a6373
Igweachi Jesse <104082049+obaikoko@users.noreply.github.com>
2023-04-12 09:56:09 +0100

    Update authService.js

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'https://goalsetter-h114.onrender.com/api/users/';
+const API_URL = 'https://goalsetter-h114.onrender.com/api/users';
 
 const register = async (userData) => {
   const response = await axios.post(API_URL, userData);
@@ -9,7 +9,7 @@ const register = async (userData) => {
   return response.data;
 };
 const login = async (userData) => {
-  const response = await axios.post(API_URL + 'login', userData);
+  const response = await axios.post(API_URL + '/login', userData);
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data));
   }
