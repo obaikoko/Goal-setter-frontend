70f9324726c4dca1e67247fc89d9d88e6fb304e2
Igweachi Jesse <104082049+obaikoko@users.noreply.github.com>
2023-04-16 11:16:24 +0100

    Update goalService.js

--- src/features/goals/goalService.js
+++ src/features/goals/goalService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://goalsetter-h114.onrender.com/api/goals';
+const API_URL = '/api/goals';
 
 const createGoal = async (goalData, token) => {
   const config = {
